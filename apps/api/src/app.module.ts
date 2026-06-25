d6e502c99a33e3c7eeeb999f637e7e6d4d4f0c5d
Harsha Vardhan <harshadanthu50@gmail.com>
2026-06-25 12:06:54 +0530

    Implemented leave apply API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./modules/auth/auth.module";
 import { DocumentsModule } from "./modules/documents/documents.module";
+import { LeavesModule } from "./modules/leaves/leaves.module";
 import { HealthModule } from "./modules/health/health.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { RedisModule } from "./redis/redis.module";
@@ -23,6 +24,7 @@ import { OrganizationModule } from "./modules/organization/organization.module";
     EmployeesModule,
     OrganizationModule,
     DocumentsModule,
+    LeavesModule,
   ],
 })
 export class AppModule { }
--- apps/api/src/modules/leaves/leaves.controller.ts
+++ apps/api/src/modules/leaves/leaves.controller.ts
@@ -1,1 +1,20 @@
-export {}
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ApplyLeaveDto } from './dto/apply-leave.dto';
+import { LeavesService } from './leaves.service';
+
+@Controller('leaves')
+export class LeavesController {
+
+  constructor(private readonly leaveService: LeavesService) {}
+
+  @Get()
+getLeaves(): any {
+  return this.leaveService.getLeaves();
+}
+
+@Post('apply')
+applyLeave(@Body() data: ApplyLeaveDto): Promise<any> {
+  return this.leaveService.applyLeave(data);
+}
+
+}
